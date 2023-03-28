9ef4a3bd4bd45b76475a59e96efdd2275495b48f
Vito Genovese <genovese.work@protonmail.com>
2023-03-28 15:56:41 +0200

    last changes 'cause changes in the API contract

--- views/web/routes.py
+++ views/web/routes.py
@@ -17,12 +17,20 @@ def r_query(model):
                     result += f"{k}={v}&"
             return result[0:-1]
         args = get_args(request.args)
-        print(model)
-        res = req.get(f"{API_URL}/{models.get(model)}?{args}").json()
-        print(res["data"])
+        try:
+            url = f"{API_URL}/{model}?{args}"
+            print(url)
+            res = req.get(url).json()
+            print(res)
+        except Exception as e:
+            res = None
         return render_template(f"{model}.html", data=res)
     return render_template(f"{model}.html")
 
 @web.route("/")
 def r_home():
     return render_template("query.html")
+
+@web.route("/<template>")
+def r_template(template):
+    return render_template(f"{template}.html")
