e87f14e927457c1bd548748b3d8e97a69b2bc7d7
Vito Genovese <genovese.work@protonmail.com>
2023-04-04 15:55:55 +0200

    last on web

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,12 +1,22 @@
 const options = {"per":['id', 't_001', 't_024', 't_046', 't_100', 't_368', 't_370', 't_372', 't_373', 't_374', 't_375', 't_377', 't_400', 't_500', 't_510', 't_667', 't_670', 't_678', 'id_BNE', 'otros_identificadores', 'fecha_nacimiento', 'fecha_muerte', 'nombre_de_persona', 'otros_atributos_persona', 'lugar_nacimiento', 'lugar_muerte', 'pais_relacionado', 'otros_lugares_relacionados', 'lugar_residencia', 'campo_actividad', 'grupo_o_entidad_relacionada', 'ocupacion', 'genero', 'lengua', 'otros_nombres', 'persona_relacionada', 'nota_general', 'fuentes_de_informacion', 'otros_datos_biograficos', 'obras_relacionadas_en_el_catalogo_BNE'],
-                "geo":['id', 't_001', 't_024', 't_034', 't_080', 't_151', 't_451', 't_510', 't_550', 't_551', 't_667', 't_670', 't_781', 'id_BNE', 'otros_identificadores', 'coordenadas_lat_lng', 'CDU', 'nombre_de_lugar', 'otros_nombres_de_lugar', 'entidad_relacionada', 'materia_relacionada', 'lugar_relacionado', 'nota_general', 'fuentes_de_informacion', 'lugar_jerarquico', 'obras_relacionadas_en_el_catalogo_BNE'],
-                "mon":[]
+                "geo":['id','id_BNE', 'otros_identificadores', 'coordenadas_lat_lng', 'CDU', 'nombre_de_lugar', 'otros_nombres_de_lugar', 'entidad_relacionada', 'materia_relacionada', 'lugar_relacionado', 'nota_general', 'fuentes_de_informacion', 'lugar_jerarquico', 'obras_relacionadas_en_el_catalogo_BNE','id', 't_001', 't_024', 't_034', 't_080', 't_151', 't_451', 't_510', 't_550', 't_551', 't_667', 't_670', 't_781'],
+                "mon":['id', 't_001', 't_035', 't_040', 't_100', 't_130', 't_245', 't_260', 't_300', 't_500', 't_899', 't_927', 't_980', 't_994']
 };
 
-let counter = 0;
+let li_counter = 0;
+
+const navigate_ul = (ul) => {
+    const lis = Array(...ul.getElementsByTagName("li"));
+    // lis.at(li_counter).style.backgroundColor = "red";
+    return lis.at(li_counter).innerHTML;
+};
+
+
 const show_ul = (input) => {
+    
     const dataset_to_search = dataset.value;
     const dropdown = document.createElement("ul");
+    dropdown.setAttribute("add",`navigate_ul(this)`);
     const deleter = () => {
         const input_container = input.parentElement.parentElement;
         const uls = Array(...input_container.getElementsByTagName("ul"));
@@ -17,7 +27,6 @@ const show_ul = (input) => {
     };
     const input_container = deleter();
     const ss = input.value;
-    counter ++;
     dropdown.className = "list-group position-absolute";
     dropdown.style = "top:90%"
     options[dataset_to_search].forEach((li) => {
@@ -29,5 +38,14 @@ const show_ul = (input) => {
         };
     });
     input_container.appendChild(dropdown);
-    setTimeout(() => deleter(), 3500);
+    if (window.event.keyCode == "40") {
+        const input_container = input.parentElement.parentElement;
+        const uls = Array(...input_container.getElementsByTagName("ul"));
+        if (uls.length >= 1) {
+            const ul = input_container.removeChild(uls[0]);
+            input.value = navigate_ul(ul);
+        };
+        // return;
+    }
+    // setTimeout(() => deleter(), 3500);
 };
--- static/js/query_builder.js
+++ static/js/query_builder.js
@@ -24,16 +24,10 @@ const trash_filter = (button) => {
     };
 };
 
-const base_url = "http://127.0.0.1:3000/api"
+const base_url = "http://139.162.185.83:3000/api"
 let query;
 
-const get_data = async(url) => {
-    const res = await fetch(url);
-    const data = await res.json();
-    return data;
-};
-
-const show_data = async() => {
+const get_data = async() => {
     const dataset = document.querySelector("#dataset").value;
     const limit = document.querySelector("#limit").value;
     const view = document.querySelector("#view").value;
@@ -46,8 +40,51 @@ const show_data = async() => {
         url += `&${filters}`
     };
     console.log(url)
-    const data = await get_data(url);
-    console.log(data);
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+};
+
+const show_data = async() => {
+    const results = document.querySelector("#results");
+    results.innerHTML = "";
     
+    const data = await get_data();
+    if (!data.success) {
+        return
+    };
+    const records = data.data.slice(0,10);
+    `
+    <ul class="list-group list-group-horizontal">
+  <li class="list-group-item">An item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+</ul>`;
+    console.log(records)
+
+    const ul_k = document.createElement("ul");
+        ul_k.className = "list-group list-group-horizontal";
+        for (const [k,v] of Object.entries(records[0])) {
+            const li = document.createElement("li");
+            li.className = "list-group-item";
+            li.innerHTML = k;
+            ul_k.appendChild(li);
+        };
+        document.querySelector("#results").appendChild(ul_k);
+    
+    records.forEach((record) => {
+
+        const ul = document.createElement("ul");
+        ul.className = "list-group list-group-horizontal";
+        for (const [k,v] of Object.entries(record)) {
+            const li = document.createElement("li");
+            li.className = "list-group-item";
+            li.innerHTML = v;
+            ul.appendChild(li);
+        };
+        results.appendChild(ul);
+    });
+
+
 }
 
